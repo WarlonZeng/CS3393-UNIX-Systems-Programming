cf23654f21a980dfeb57b1225040107d41fcbb19
Warlon Zeng <wz634@nyu.edu>
2017-05-19 13:44:21 -0400

    end

--- tests2/barrier.h
+++
@@ -1,9 +0,0 @@
-struct Barrier {
-    int count;
-    pthread_mutex_t mutex;
-    pthread_cond_t condvar;
-};
-
-void init_barrier(struct Barrier* barP, int n);
-void wait_barrier(struct Barrier* barP);
-void release_barrier(struct Barrier* barP);
